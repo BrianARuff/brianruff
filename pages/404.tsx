faa7f765a6a48afbfc516567120eec85c9622047
Brian Ruff <briananthonyruff@gmail.com>
2022-05-12 20:06:09 -0400

    updated meta tags, added 404 page, using custom _document.tsx file with 'en' added to html tag

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+function Custom404() {
+  return (
+    <div>
+      <h1>404: Page Not Found 🙁</h1>
+      <p>
+        Sorry, I do not have the sub-domain you searched on the site. In fact,
+        as of <time dateTime="2022-05-12">this date</time>, I do not have any
+        sub-domains. Check back in the future, maybe I will then.
+      </p>
+    </div>
+  );
+}
+
+export default Custom404;
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,56 @@ export default function Home() {
         src={"/small-me.jpg"}
       />
       <Head>
-        <title>Brian Ruff</title>
-        <meta name="description" content="Brian Ruff's Website" />
-        <link rel="icon" href={"/small-me.jpg"} />
+        <Head>
+          <meta name="robots" content="follow" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <meta property="og:title" content="Brian Ruff&apos;s Portfolio" />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content="https://brianruff.com" />
+          <meta
+            property="og:image"
+            content="https://pbs.twimg.com/profile_images/1519832143500914694/9zX3itOy_200x200.jpg"
+          />
+
+          <meta
+            property="og:description"
+            content="Get to know me and my work."
+          />
+          <meta property="og:site_name" content="Brian Ruff" />
+          <meta charSet="utf-8" />
+          <meta name="author" content="Brian Ruff" />
+          <meta name="copyright" content="Brian Ruff" />
+          <meta property="og:locale" content="en_US" />
+
+          <meta property="og:image:width" content="200" />
+          <meta property="og:image:height" content="200" />
+          <meta property="og:image:type" content="image/jpeg" />
+          <meta property="og:image:alt" content="Brian Ruff" />
+          <meta
+            property="og:audio"
+            content="https://www.youtube.com/watch?v=04854XqcfCY"
+          />
+          <meta
+            property="og:video"
+            content="https://www.youtube.com/watch?v=04854XqcfCY"
+          />
+          <meta property="og:video:width" content="640" />
+          <meta property="og:video:height" content="360" />
+          <meta property="og:video:type" content="video/mp4" />
+          <meta property="og:video:alt" content="Queen: We are the Champions" />
+          <meta
+            property="og:video:url"
+            content="https://www.youtube.com/watch?v=04854XqcfCY"
+          />
+          <meta
+            property="og:video:secure_url"
+            content="https://www.youtube.com/watch?v=04854XqcfCY"
+          />
+          <link rel="icon" href={"/small-me.jpg"} />
+          <title>Brian Ruff&apos;s Portfolio</title>
+        </Head>
       </Head>
       <p>
         I&apos;m not 100% sure what I want to do with this yet, so this is
